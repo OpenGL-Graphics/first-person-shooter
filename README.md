7687837af7288f3a733791e797e40ae90beefb05
h4k1m0u <h.benoudjit@gmail.com>
2020-11-02 13:36:16 +0100

    Draw a triangle with OGL vertex & fragment shaders

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
+# Program
+- **src/main.cpp:** Draws a triangle using OpenGL shaders on a window generated with GLFW.
+
 # OpenGL
 - **GLFW (Graphics Library Framework):** Library for creating windows with OpenGL context and for handling mouse and keyboard interactions. Read this [GLFW tutorial].
 - **Glad:** Loader for OpenGL functions. Following this tutorial from [learnopengl.com], glad files can be generated using this [webservice] then copied inside include and src folders.
 
-# Credit
-- [GLFW tutorial] 
-- [learnopengl.com]
-
 [GLFW tutorial]: https://www.glfw.org/docs/latest/quick_guide.html
-[learnopengl.com]: https://learnopengl.com/Getting-started/Creating-a-window
 [webservice]: https://glad.dav1d.de/
+
+# Shaders
+Shaders are written in GLSL (OpenGL Shading Language). The following two shaders (GPU programs) are needed to draw with OpenGL:
+
+- **Vertex shader:** for each vertex, determines its position on the scene.
+- **Fragment shader:** for each pixel, determines its color on the screen.
+
+# Credit
+Useful tutorials:
+
+- Getting started with GLFW: [GLFW tutorial] 
+- Learn OpenGL tutorial: [learnopengl.com]
+- Opengl-tutorial: [opengl-tutorial.org]
+- OpenGL tutorial series on Youtube: [TheCherno]
+- Similarly to this tutorial, an older version of GLSL was used: [open.gl]
+
+[learnopengl.com]: https://learnopengl.com/Getting-started/Creating-a-window
+[opengl-tutorial.org]: http://www.opengl-tutorial.org/beginners-tutorials/
+[TheCherno]: https://www.youtube.com/watch?v=W3gAzLwfIP0
+[open.gl]: https://open.gl/
+
+# Docs
+- [khronos.org]
+- [docs.gl]
+
+[docs.gl]: http://docs.gl/
+[khronos.org]: https://www.khronos.org/registry/OpenGL-Refpages/gl4/
---
+++ src/main.cpp
@@ -0,0 +1,166 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <vector>
+
+static void on_key(GLFWwindow* window, int key, int scancode, int action, int mods) {
+  // close window on escape key press
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+  }
+}
+
+static GLuint create_shaders(const std::string& source_vertex, const std::string& source_fragment) {
+  // The Cherno: https://www.youtube.com/watch?v=71BLZwRGUJE
+  const char* source_vertex_char = source_vertex.c_str();
+  const char* source_fragment_char = source_fragment.c_str();
+
+  // compile shaders
+  GLuint shader_vertex= glCreateShader(GL_VERTEX_SHADER);
+  GLuint shader_fragment= glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(shader_vertex, 1, &source_vertex_char, NULL);
+  glShaderSource(shader_fragment, 1, &source_fragment_char, NULL);
+  glCompileShader(shader_vertex);
+  glCompileShader(shader_fragment);
+
+  // TODO: group both shaders in same function (refactoring)
+  // error handling for vertex shader compilation
+  GLint result1;
+  glGetShaderiv(shader_vertex, GL_COMPILE_STATUS, &result1);
+  if (result1 == GL_FALSE) {
+    GLint length1;
+    glGetShaderiv(shader_vertex, GL_INFO_LOG_LENGTH, &length1);
+    std::vector<GLchar> message(length1);
+    glGetShaderInfoLog(shader_vertex, length1, NULL, message.data());
+    std::cout << "Vertex shader: " << message.data() << "\n";
+
+    glDeleteShader(shader_vertex);
+    return 0;
+  }
+
+  // error handling for fragment shader compilation
+  GLint result2;
+  glGetShaderiv(shader_fragment, GL_COMPILE_STATUS, &result2);
+  if (result2 == GL_FALSE) {
+    GLint length2;
+    glGetShaderiv(shader_fragment, GL_INFO_LOG_LENGTH, &length2);
+    std::vector<GLchar> message(length2);
+    glGetShaderInfoLog(shader_fragment, length2, NULL, message.data());
+    std::cout << "Fragment shader: " << message.data() << "\n";
+
+    glDeleteShader(shader_fragment);
+    return 0;
+  }
+
+  // attach shaders to program & link it
+  GLuint program = glCreateProgram();
+  glAttachShader(program, shader_vertex);
+  glAttachShader(program, shader_fragment);
+  glLinkProgram(program);
+  glValidateProgram(program);
+
+  // error handling of program linking
+  GLint result_link;
+  glGetProgramiv(program,  GL_LINK_STATUS, &result_link);
+  if (result_link == GL_FALSE) {
+    GLint length_link;
+    glGetProgramiv(program,  GL_INFO_LOG_LENGTH, &length_link);
+    std::vector<GLchar> message(length_link);
+    glGetProgramInfoLog(program, length_link, NULL, message.data());
+    std::cout << "program: " << message.data() << "\n";
+
+    glDeleteProgram(program);
+    return 0;
+  }
+
+  // flag attached shaders objects for deletion
+  glDeleteShader(shader_vertex);
+  glDeleteShader(shader_fragment);
+
+  return program;
+}
+
+int main() {
+  // initialize glfw
+  if (!glfwInit()) {
+    std::cout << "Failed to initialize GLFW" << "\n";
+    return 1;
+  }
+
+  // create window and OpenGL context
+  GLFWwindow* window = glfwCreateWindow(640, 480, "Window title", NULL, NULL);
+  if (!window) {
+    std::cout << "Failed to create window or OpenGL context" << "\n";
+    return 1;
+  }
+
+  // initialize glad before calling gl functions
+  glfwMakeContextCurrent(window);
+  if (!gladLoadGL()) {
+    std::cout << "Failed to load Glad (OpenGL)" << "\n";
+    return 1;
+  } else {
+    std::cout << "Opengl version: " << glGetString(GL_VERSION) << "\n";
+    std::cout << "GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << "\n";
+  }
+
+  // callback for processing key press
+  glfwSetKeyCallback(window, on_key);
+
+  // GPU buffer for vertexes positions
+  const float positions[6] = {
+    -0.5, -0.5,
+     0,    0.5,
+     0.5, -0.5
+  };
+  GLuint index_buffer;
+  glGenBuffers(1, &index_buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, index_buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+  // buffer attributes (position, color...)
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *) 0);
+  glEnableVertexAttribArray(0);
+
+  // shader source codes (newer GLSL version not supported)
+  std::string source_vertex = 
+    "#version 130\n"
+    "in vec2 position;\n"
+    "void main()\n"
+    "{\n"
+    "  gl_Position = vec4(position, 0.0, 1.0);\n"
+    "}\n";
+
+  std::string source_fragment = 
+    "#version 130\n"
+    "out vec4 color;\n"
+    "void main()\n"
+    "{\n"
+    "  color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+    "}\n";
+
+  // create then install vertex & fragment shaders on GPU
+  GLuint program = create_shaders(source_vertex, source_fragment);
+  glUseProgram(program);
+
+  // main loop
+  while (!glfwWindowShouldClose(window)) {
+    // clear buffer with blue color
+    glClearColor(0.0f, 0.5f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // draw triangle from bound buffer
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    // process events & show rendered buffer
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
+
+  // destroy shaders, window & terminate glfw
+  glDeleteProgram(program);
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
